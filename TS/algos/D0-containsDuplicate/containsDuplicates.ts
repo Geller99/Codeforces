01629c861c5edde18646b20c8bba2c24d2c98f17
geller99 <gellera800@gmail.com>
2025-01-04 15:49:56 -0600

    Contains Duplicate

---
+++ TS/algos/D0-containsDuplicate/containsDuplicates.ts
@@ -0,0 +1,24 @@
+
+
+
+/**
+ * 
+ * @param nums 
+ * 
+ * Given an array, return true if it contains any duplicate values
+ * or false if it doesn't
+ */
+
+const containsDuplicate = (nums: number[]): boolean => {
+    if (nums.length <= 1) return false;
+
+    let checker = new Set();
+
+    for(let num in nums) {
+        if(checker.has(num)) return true;
+        else checker.add(num);
+    }
+
+    return false;
+
+}
