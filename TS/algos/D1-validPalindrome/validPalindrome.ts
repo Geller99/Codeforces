c5a423cadf3ee3cc0f7b3ba61d9922df47c940bc
geller99 <gellera800@gmail.com>
2025-01-08 12:18:54 -0600

    Setup TS config with jest

---
+++ TS/algos/D1-validPalindrome/validPalindrome.ts
@@ -0,0 +1,26 @@
+
+
+
+/**
+ * 
+ * @param nums
+ * 
+ * Goal is to check if a valid string is a palindrome or not
+ */
+
+const validPalindrome = (str: string) => {
+    if (str.length === 0) return true;
+
+    str = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let left = 0;
+    let right = str.length - 1;
+
+    while(left < right) {
+        if (str[left] !== str[right]) return false
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
